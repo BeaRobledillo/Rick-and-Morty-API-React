d489d9510cd6147dbc5cf9c85e7ea8e763af1e3d
BeitxuelaWEB <bearobledillogomez@gmail.com>
2022-06-29 08:43:57 +0200

    add screenshot

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Navbar from "./components/Navbar";
+import Characters from "./components/Characters";
+import Pagination from "./components/Pagination";
 
 function App() {
+
+  const [characters, setCharacters] = useState([]);
+  const [info, setInfo] = useState({});
+
+  const initialUrl = "https://rickandmortyapi.com/api/character";
+
+  const fetchCharacters = (url) => {
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        setCharacters(data.results);
+        setInfo(data.info);
+      })
+      .catch(error => console.log(error))
+
+  };
+
+  const onPrevious = () => {
+    fetchCharacters(info.prev);
+  }
+
+  const onNext = () => {
+    fetchCharacters(info.next);
+  }
+
+  useEffect(() => {
+    fetchCharacters(initialUrl);
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar brand="Rick and Morty App" />
+
+      <div className="container mt-5">
+        <Pagination
+          prev={info.prev}
+          next={info.next}
+          onPrevious={onPrevious}
+          onNext={onNext}
+        />
+        <Characters characters={characters} />
+        <Pagination
+          prev={info.prev}
+          next={info.next}
+          onPrevious={onPrevious}
+          onNext={onNext}
+        />
+      </div>
+    </>
   );
+
 }
 
 export default App;
---
+++ src/components/Pagination.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Pagination = ({ prev, next, onPrevious, onNext }) => {
+  const handlePrevious = () => {
+    onPrevious();
+  };
+
+  const handleNext = () => {
+    onNext();
+  };
+
+  return (
+    <nav className="my-5">
+      <ul className="pagination justify-content-center">
+        {prev ? (
+          <li className="pag-item">
+            <button className="page-link" onClick={handlePrevious}>
+              Previous
+            </button>
+          </li>
+        ) : null}
+        {next ? (
+          <li className="pag-item">
+            <button className="page-link" onClick={handleNext}>
+              Next
+            </button>
+          </li>
+        ) : null}
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination;
